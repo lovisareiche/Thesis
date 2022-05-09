93fb318d3abe9149bbdb8fa2621e9041217f6883
lovisareiche <104841377+lovisareiche@users.noreply.github.com>
2022-05-09 15:43:05 +0100

    Update gower_distfun.m

--- Empirical/clustering/gower_distfun.m
+++ Empirical/clustering/gower_distfun.m
@@ -6,16 +6,17 @@ function D2 = gower_distfun(ZI,ZJ)
 
 % n is number of observations
 % p is number of variables
-
+% Input:
 % ZI is a 1-by-p vector containing a single observation.
 % ZJ is an (n-1)-by-p matrix containing multiple observation. distfun must accept a matrix ZJ with an arbitrary number of observations.
+% Output:
 % D2 is an p2-by-1 vector of distances, and D2(k) is the distance between observations ZI and ZJ(k,:).
 
 p = size(ZI,2);
 
 % calculate matrix of partial dissimilarity
 
-load minmax.mat % pre-saved vectors for minimum and maximum values of the p variables
+load minmax.mat % pre-saved vectors for minimum and maximum values of the p variables, must be saved outside this function depending on the variables the clustering occurs on
 MPD=abs(ZI-ZJ)./abs(Final_p_max-Final_p_min);
 
 % calculate distance as weighted average
